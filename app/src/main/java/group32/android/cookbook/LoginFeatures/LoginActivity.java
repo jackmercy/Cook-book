8e200907499507b57d81984682602bef7a4536e3
Jack <khoicaominh.mmt@gmail.com>
2017-12-06 16:26:27 +0700

    no message

--- app/src/main/java/group32/android/cookbook/LoginFeatures/LoginActivity.java
+++ app/src/main/java/group32/android/cookbook/LoginFeatures/LoginActivity.java
@@ -183,6 +183,11 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(LoginActivity.this, "Authentication failed.",
                             Toast.LENGTH_SHORT).show();
                 }
+                else{
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
 
             }
         });
